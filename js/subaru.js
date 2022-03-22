3cde2629a314a683a34572d11bb1404119d2d52d
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-03-22 15:05:06 +0300

    add jquery

---
+++ js/subaru.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#category").click(function(){
+        $("#hdlight").show('1500');
+        $("#rimwheel").show('1500');
+        $("#pipeexhaust").show('1500');
+        $("#mirrorside").show('1500');
+    });
+    
+    
+    })
